99f95dcf1aea32399d2c35d581a740d88bc4c6d9
Luthfan123 <54345954+Luthfan123@users.noreply.github.com>
2020-11-19 19:23:15 +0530

    Add files via upload

---
+++ squarearea.py
@@ -0,0 +1,52 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "enter the side of the square: 22\n",
+      "area of square is  484\n"
+     ]
+    }
+   ],
+   "source": [
+    "side=int(input(\"enter the side of the square: \"))\n",
+    "area=side*side\n",
+    "print(\"area of square is \",area)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
